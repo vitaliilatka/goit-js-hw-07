f82621ae3b1fc88bee795f31e5d13a067c176199
vetallatka <latka.vitalik@yandex.ru>
2020-12-20 14:55:55 +0100

    correcting work task 8

--- hw-07-02.js
+++ hw-07-02.js
@@ -1,17 +1,19 @@
 const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
-];
-const createItem = item => {
-    const itemRef = document.createElement('li');
-    itemRef.textContent = item;
-    return itemRef;
-}  
-const items = ingredients.map((item) => createItem(item));
-const listRef = document.querySelector('#ingredients');
-listRef.append(...items);
-console.log(listRef);
+    "Картошка",
+    "Грибы",
+    "Чеснок",
+    "Помидоры",
+    "Зелень",
+    "Приправы",
+  ];
+  
+  const createItem = item => {
+      const itemRef = document.createElement("li");
+      itemRef.textContent = item;
+      return itemRef;
+  };
+   
+  const items = ingredients.map((item) => createItem(item));
+  
+  const listRef = document.querySelector('#ingredients');
+  listRef.append(...items);
--- hw-07-08.js
+++ hw-07-08.js
@@ -5,10 +5,10 @@ const boxesRef = document.querySelector('#boxes');
 
 let boxes = [];
 const randomRgb = () => {
-    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-    const r = randomBetween(0, 255);
-    const g = randomBetween(0, 255);
-    const b = randomBetween(0, 255);
+    const randomBetween = Math.floor(Math.random() * 256);
+    const r = randomBetween();
+    const g = randomBetween();
+    const b = randomBetween();
     const rgb = `rgb(${r}, ${g}, ${b})`;
     return rgb;
 };
